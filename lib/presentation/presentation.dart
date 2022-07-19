cf772bad7615492ff8a483eaa647f2c33b276209
Dominik Wawrzonek <dwawrzonek@ics.com>
2022-07-19 12:33:21 +0200

    Change systemUIOverlayStyle for android

--- lib/presentation/presentation.dart
+++ lib/presentation/presentation.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:pajacyk/presentation/application/application.dart';
 import 'package:pajacyk/presentation/application/theme.dart';
 
@@ -9,6 +11,15 @@ import '../domain/notification_assistant/notification_assistant.dart';
 Future<void> runApplication() async {
   WidgetsFlutterBinding.ensureInitialized();
   NotificationApi.init();
+
+  if (Platform.isAndroid) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor: Colors.black87,
+      //systemNavigationBarIconBrightness: Brightness.light
+    ));
+    //   SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+    //       overlays: [SystemUiOverlay.top]);
+  }
 
   runZonedGuarded(
     () {
